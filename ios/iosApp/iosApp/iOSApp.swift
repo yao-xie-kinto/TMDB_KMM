1385b15d73cb868747fdc40fb2b7631ffd51a868
Yao <yao.xie@kinto-technologies.com>
2023-02-26 23:31:19 +0900

    Update

--- ios/iosApp/iosApp/iOSApp.swift
+++ ios/iosApp/iosApp/iOSApp.swift
@@ -6,7 +6,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        let controller = AvoidDispose(RootViewControllersKt.RootViewController())
+        let controller = AvoidDispose(RootViewControllersKt.getRootViewController())
         controller.view.backgroundColor = .white
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.backgroundColor = .white
